d14a05c566272a0c2da23617178d8a48ccc6bf96
dim0nster <32572277+dim0nster@users.noreply.github.com>
2024-01-14 23:16:35 +0300

    4. Конфигурация контейнера [#505072 #473575]

--- src/main/java/ru/job4j/di/annotation/Bird.java
+++ src/main/java/ru/job4j/di/annotation/Bird.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Bird implements Pet<Bird>{
+public class Bird implements Pet<Bird> {
     @Value("${bird.name:simpleBird}")
     private String name;
 
--- src/main/java/ru/job4j/di/annotation/Dog.java
+++ src/main/java/ru/job4j/di/annotation/Dog.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class Dog implements Pet<Dog> {
     private String name = "Dog";
-    public Dog() {}
+    public Dog() { }
 
     public Dog(String name) {
         this.name = name;
